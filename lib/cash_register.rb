a9409b4a097f7469174282c65b669e62c36b4783
Christopher Luna <chris@illusionlayervfx.com>
2019-06-15 04:29:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ attr_accessor :total, :discount, :item, :last_transaction
   def apply_discount
     if @discount > 0
       @discount = @discount/100.to_f
-      @total = @total - ()@total * @discount)
+      @total = @total - (@total * @discount)
     else
    end
   end
