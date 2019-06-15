e8e59ec9ba20614cceac4cb0398765751f2055cc
Christopher Luna <chris@illusionlayervfx.com>
2019-06-15 04:51:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,8 +11,16 @@ attr_accessor :total, :discount, :item, :last_transaction
   end
 
   def add_item(title, price, quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
     self.total += price * quantity
-    @items << title
   end
 
   def apply_discount
