875c6d4920e19f635208e302e40d724c81f7a287
Christopher Luna <chris@illusionlayervfx.com>
2019-06-15 03:57:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class CashRegister
 
-attr_accessor :total, :discount, :item, :laast_transaction
+attr_accessor :total, :discount, :item, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
